2c7caffb1151c5f97069ffd592865ae6fc1d5688
init <79251139+senor-init@users.noreply.github.com>
2021-04-23 04:25:01 +0100

    Update command_handler.lua
    i know it sucks don't bully me

--- deps/command_handler.lua
+++ deps/command_handler.lua
@@ -2,16 +2,16 @@ local fs = require("fs")
 local commands = {}
 
 for _, dirs in pairs(fs.readdirSync("./commands")) do
-    for _, file in pairs(fs.readdirSync("./commands/"..dirs)) do
+    for _, file in pairs(fs.readdirSync("./commands/" .. dirs)) do
         if file:match("(.+)%.lua") then --matches lua files only
-            commands[dirs]={}
+            commands[dirs] = {}
             local Command_Name = string.sub(file, 1, #file - 4) --removes .lua from the file name
-            
-            commands[dirs][Command_Name] = require(".commands."..(dirs or "general")..".".. Command_Name) --requires the files
+
+            commands[dirs][Command_Name] = require(".commands." .. (dirs or "general") .. "." .. Command_Name) --requires the files
             if type(commands[dirs][Command_Name]) ~= "function" then
                 commands[dirs][Command_Name] = nil
             end
-         --accepts only files that returned a function and removes everything else
+        --accepts only files that returned a function and removes everything else
         end
     end
 end
