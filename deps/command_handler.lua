1e6dc043d6713d51dd021b286e93f2d6d4797413
init <79251139+senor-init@users.noreply.github.com>
2021-04-23 14:06:37 +0100

    Update command_handler.lua

--- deps/command_handler.lua
+++ deps/command_handler.lua
@@ -7,7 +7,6 @@ for _, dirs in pairs(fs.readdirSync("./commands")) do
     for _, file in pairs(fs.readdirSync("./commands/" .. dirs)) do
         if file:match("(.+)%.lua") then --matches lua files only
             local Command_Name = string.sub(file, 1, #file - 4) --removes .lua from the file name
-            print(Command_Name)
 
             commands[dirs][Command_Name] = require(".commands." .. dirs .. "." .. Command_Name) --requires the files
             if type(commands[dirs][Command_Name]) ~= "function" then
