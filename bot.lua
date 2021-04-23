8c7872ca411410ab0ea66107bbbef354d7f8c828
init <79251139+senor-init@users.noreply.github.com>
2021-04-23 03:06:46 +0100

    h

---
+++ bot.lua
@@ -0,0 +1,22 @@
+local discordia=require("discordia")
+local client=discordia.Client()
+local cmds=require("command_handler")
+local prefix="s!"
+
+local token=""
+
+client:on("messageCreate", 
+    function(message)
+        if string.sub(message.content,1,#(prefix))==prefix then
+            for _,i in pairs(cmds) do
+                if i[string.sub(message.content,#(prefix)+1)] then
+                    i[string.sub(message.content,#(prefix)+1)](message,client)
+                end
+            end
+        end
+
+    end)
+
+
+
+client:run("Bot "..token)
