e6f1dbf8f1b8fd5e8d79530388d2216514f70ad4
init <79251139+senor-init@users.noreply.github.com>
2021-04-23 12:34:23 +0100

    fixed bug in  the command handler where it returns one file

--- deps/command_handler.lua
+++ deps/command_handler.lua
@@ -2,16 +2,18 @@ local fs = require("fs")
 local commands = {}
 
 for _, dirs in pairs(fs.readdirSync("./commands")) do
+    commands[dirs] = {}
+
     for _, file in pairs(fs.readdirSync("./commands/" .. dirs)) do
         if file:match("(.+)%.lua") then --matches lua files only
-            commands[dirs] = {}
             local Command_Name = string.sub(file, 1, #file - 4) --removes .lua from the file name
+            print(Command_Name)
 
-            commands[dirs][Command_Name] = require(".commands." .. (dirs or "general") .. "." .. Command_Name) --requires the files
+            commands[dirs][Command_Name] = require(".commands." .. dirs .. "." .. Command_Name) --requires the files
             if type(commands[dirs][Command_Name]) ~= "function" then
-                commands[dirs][Command_Name] = nil
+
+            --accepts only files that returned a function and removes everything else
             end
-        --accepts only files that returned a function and removes everything else
         end
     end
 end
