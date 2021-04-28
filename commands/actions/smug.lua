45190c0ddd0410edde9f7d99c9fd83c328c95005
init <79251139+senor-init@users.noreply.github.com>
2021-04-28 16:19:47 +0100

    Update smug.lua

--- commands/actions/smug.lua
+++ commands/actions/smug.lua
@@ -4,17 +4,25 @@ local json = require("deps.json")
 return function(...)
     local message, client = ...
     local user = client:getUser(message.content:match("(%d+)"))
-    if message.mentionedUsers.first or user then
-        local kiss = json.parse(spawn("curl", {args = {"http://api.nekos.fun:8080/api/smug"}, stdio = {nil, true, 1}}).stdout.read())
-        local responses={" is embarrassing "," is showing love to "," is doing stuff to "}
-        message:reply {
-            embed = {
-                title =message.author.name .. responses[math.random(#responses)] .. (user.name or message.mentionedUsers.first.name),
-                image = {url = kiss.image},
-                color=16777214
-            }
+    local kiss =
+        json.parse(spawn("curl", {args = {"http://api.nekos.fun:8080/api/smug"}, stdio = {nil, true, 1}}).stdout.read())
+    local responses
+    function with()
+        local them = (((message.mentionedUsers.first or {}).name or (user or {}).name or "") or "")
+        if them ~= "" then
+            responses={" is smirking at "," is lewding "," is smugging at "}
+            return responses[math.random(#responses)]..them
+            else
+            resonses={" is smugging "," is smirking "," is thinking lewd "}
+            return responses[math.random(#responses)]
+        end
+     end
+    message:reply {
+        embed = {
+            title = message.author.name .. responses[math.random(#responses)] .. with(),
+            image = {url = kiss.image},
+            color = 16777214
         }
-    else
-        message:reply("mention someone to use this command")
-    end
+    }
 end
+
