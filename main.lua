2c47444c25a03e2422a65ec6466f7170ee4a2517
init <79251139+senor-init@users.noreply.github.com>
2021-04-30 13:31:13 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -7,7 +7,7 @@ require("reqs.functions")
 local iw=require("invite_whitelister")  -- delete this if you're using Emilia for personal uses and the file deps/invite_whitelister.lua as well
 
 client:on("messageCreate",function(message) 
-    if message.guild and not message.guild:getMember(message.author.id):hasRole("773838374440009758") then
+    if message.guild and not message.author.bot and not message.guild:getMember(message.author.id):hasRole("773838374440009758") then
         iw(message) 
     end 
 end)-- ^
@@ -24,7 +24,7 @@ client:on(
     "messageCreate",
     function(message)
         local arg = string.split(message.content, " ")[1]
-        if string.sub(arg, 1, #(prefix)) == prefix and message.guild then
+        if string.sub(arg, 1, #(prefix)) == prefix and message.guild and not message.author.bot then
             for _, i in pairs(cmds) do
                 if i[string.sub(arg, #(prefix) + 1)] then
                     i[string.sub(arg, #(prefix) + 1)](message, client, {commands = cmds, prefix = prefix})
