e30247d1f8da63637b24839b4b86d8f1cb4cb4b6
init <79251139+senor-init@users.noreply.github.com>
2021-05-05 07:18:54 +0100

    more action commands

---
+++ commands/actions/pout.lua
@@ -0,0 +1,28 @@
+local spawn = require("deps.coro-spawn")
+local json = require("deps.json")
+
+return function(...)
+    local message, client = ...
+    local user = client:getUser(message.content:match("(%d+)"))
+    local req =
+        json.parse(spawn("curl", {args = {"https://shiro.gg/api/images/pout"}, stdio = {nil, true, 1}}).stdout.read())
+    local responses
+    function with()
+        local them = (((message.mentionedUsers.first or {}).name or (user or {}).name or "") or "")
+        if them ~= "" then
+            responses={" is pouting at "," is angery because of "," is mad lol "}
+            return responses[math.random(#responses)]..them
+            else
+                responses={" is pouting "," is angery "," is mad "}
+            return responses[math.random(#responses)]
+        end
+     end
+    message:reply {
+        embed = {
+            title = message.author.name .. with(),
+            image = {url = req.url},
+            color = 16777214
+        }
+    }
+end
+
