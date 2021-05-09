a79f18db14ee3f7d7e6dcece0aa2d9612607b811
init <79251139+senor-init@users.noreply.github.com>
2021-05-09 04:36:35 +0100

    Update bonk.lua

--- commands/actions/bonk.lua
+++ commands/actions/bonk.lua
@@ -5,8 +5,8 @@ return function(...)
     local message, client = ...
     local user = client:getUser(message.content:match("(%d+)"))
     if message.mentionedUsers.first or user then
-        local req = json.parse(spawn("curl", {args = {"https://shiro.gg/api/images/punch"}, stdio = {nil, true, 1}}).stdout.read())
-        local responses={" is yeeting "," YEETED "," yeets "}
+        local req = json.parse(spawn("curl", {args = {"https://shiro.gg/api/images/bonk"}, stdio = {nil, true, 1}}).stdout.read())
+        local responses={" bonks "," bonked "," is bonking "}
         message:reply {
             embed = {
                 title =message.author.name .. responses[math.random(#responses)] .. (user.name or message.mentionedUsers.first.name),
