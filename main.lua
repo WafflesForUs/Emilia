72cef1ba9d1bbab1c43441929a20427142dc3add
init <79251139+senor-init@users.noreply.github.com>
2021-05-12 02:54:57 +0100

    added logs for errors
    also the bot is now less likely to error due to me pcalling all message events

--- main.lua
+++ main.lua
@@ -7,27 +7,32 @@ require("reqs.functions")
 local iw=require("invite_whitelister")  -- delete this if you're using Emilia for personal uses and the file deps/invite_whitelister.lua as well
 
 client:on("messageCreate",function(message) 
-    if message.guild and not message.author.bot and not message.guild:getMember(message.author.id):hasRole("773838374440009758") then
-        iw(message) 
-    end 
+    if not message.guild or message.author.bot then return end
+    if message.guild:getMember(message.author.id):hasRole("773838374440009758") then
+        pcall(iw,message) 
+    end
 end)-- ^
 
 client:on(
     "messageCreate",
     function(message) 
         if  message.guild then
-            acv(message, client)
+            pcall(acv,message, client)
         end
     end
 )
 client:on(
     "messageCreate",
     function(message)
+        if not message.guild or message.author.bot then return end
         local arg = string.split(message.content, " ")[1]
-        if string.sub(arg, 1, #(prefix)) == prefix and message.guild and not message.author.bot then
+        if string.sub(arg, 1, #(prefix)) == prefix then
             for _, i in pairs(cmds) do
                 if i[string.sub(arg, #(prefix) + 1)] then
-                    i[string.sub(arg, #(prefix) + 1)](message, client, {commands = cmds, prefix = prefix})
+                    local h,b=pcall(i[string.sub(arg, #(prefix) + 1)],message, client, {commands = cmds, prefix = prefix})
+                    if not h then
+                        client:getChannel("841854073405702194"):send{embed={color=15158332,title="error",description="```"..b.."```",timestamp = discordia.Date():toISO('T', 'Z')}}
+                    end
                 end
             end
         end
