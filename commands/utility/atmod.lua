30c9e31fc043e2f5c6910bf363393a9b575470ab
init <79251139+senor-init@users.noreply.github.com>
2021-05-27 02:52:20 +0100

    cleaning

--- commands/utility/atmod.lua
+++ commands/utility/atmod.lua
@@ -1,34 +1,16 @@
 return function(...)
     local message, client = ...
-    function shuffle(tbl)
-        if type(tbl) ~= "table" then
-            return
-        end
-        local check = {}
-        for i, v in pairs(tbl) do
-            ::bacc::
-            local rand = math.random(#tbl)
-            if check[rand] then
-                goto bacc
-            end
-            check[rand] = v
-        end
-        return check
+    function RandomFromarray(tbl)
+        return tbl[math.random(#tbl)]
     end
-
-    function shuffleMembers(g)
+    function RandomDndOrOnlineMember(g)
         local t = {}
         for i in g:iter() do
-            table.insert(t, i)
+            if i.status == "dnd" or i.status == "online" then
+                table.insert(t, i)
+            end
         end
-        return shuffle(t)
+        return RandomFromarray(t)
     end
-    p(#client:getRole("773838374440009758").members)
-    for _, i in pairs(shuffleMembers(client:getRole("773838374440009758").members)) do
-        print(i)
-        if i.status == "dnd" or i.status == "online" then
-            return message:reply(i.mentionString)
-        end
-    end
-    message:reply("there are no online mods, please try again later")
+    message:reply(RandomDndOrOnlineMember(client:getRole("773838374440009758").members).mentionString)
 end
