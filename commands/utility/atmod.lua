33af52427e0f388fbc4d5a6242ce764041d6b6e1
init <79251139+senor-init@users.noreply.github.com>
2021-05-27 03:01:30 +0100

    last commit I swear

--- commands/utility/atmod.lua
+++ commands/utility/atmod.lua
@@ -1,17 +1,18 @@
+function RandomFromarray(tbl)
+    return tbl[math.random(#tbl)]
+end
+function RandomDndOrOnlineMember(g)
+    local t = {}
+    for i in g:iter() do
+        if i.status == "dnd" or i.status == "online" then
+            table.insert(t, i)
+        end
+    end
+    return RandomFromarray(t)
+end
+
 return function(...)
     local message, client = ...
-    function RandomFromarray(tbl)
-        return tbl[math.random(#tbl)]
-    end
-    function RandomDndOrOnlineMember(g)
-        local t = {}
-        for i in g:iter() do
-            if i.status == "dnd" or i.status == "online" then
-                table.insert(t, i)
-            end
-        end
-        return RandomFromarray(t)
-    end
     local mod=RandomDndOrOnlineMember(client:getRole("773838374440009758").members)
     if mod then 
         message:reply(mod.mentionString)
