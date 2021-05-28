aa3b69ca4970ef7b82d04466869da7a1a8051d86
init <79251139+senor-init@users.noreply.github.com>
2021-05-29 00:12:58 +0100

    Create wave.lua

---
+++ commands/actions/wave.lua
@@ -0,0 +1,20 @@
+local spawn = require("deps.coro-spawn")
+local json = require("deps.json")
+
+return function(...)
+    local message, client = ...
+    local user = client:getUser(message.content:match("(%d+)"))
+    if message.mentionedUsers.first or user then
+        local req = json.parse(spawn("curl", {args = {"https://api.waifu.pics/sfw/wave"}, stdio = {nil, true, 1}}).stdout.read())
+        local responses={" is waving to "," is saying hi to "}
+        message:reply {
+            embed = {
+                title =message.author.name .. responses[math.random(#responses)] .. (user.name or message.mentionedUsers.first.name),
+                image = {url = req.url},
+                color=16777214
+            }
+        }
+    else
+        message:reply("mention someone to use this command")
+    end
+end
