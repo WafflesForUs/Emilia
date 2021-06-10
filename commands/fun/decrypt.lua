ae83ea6ee45008e522e2bed7eb5621baa7f78fa4
init <79251139+senor-init@users.noreply.github.com>
2021-06-10 08:22:13 +0100

    h

--- commands/fun/decrypt.lua
+++ commands/fun/decrypt.lua
@@ -1,25 +1,27 @@
 function to255s(number)
-    number=tonumber(number)
-    if number>255 then
-        number=number-255
-        if number>255 then
-            number=to255s(number)
+    number = tonumber(number)
+    if number > 255 then
+        number = number - 255
+        if number > 255 then
+            number = to255s(number)
         end
     end
-    if number<0 then
-         number=number+255
-         if number<0 then
-            number=to255s(number)
-         end
+    if number < 0 then
+        number = number + 255
+        if number < 0 then
+            number = to255s(number)
+        end
     end
-return number
+    return number
 end
 
 function decrypt(a, b)
-    if not a then return end
+    if not a then
+        return
+    end
     local data = ""
     local h = tonumber(b)
-    for i in string.gmatch(a,"%d+") do
+    for i in string.gmatch(a, "%d+") do
         local bt = tonumber(i)
         if bt then
             if not h then
@@ -36,19 +38,21 @@ function decrypt(a, b)
     return data
 end
 
-return {function(...)
-    local message,client,data=...
-    local args=message.content:split(" ")
-    if args[3] then
-        print(message.content:sub(message.content:find(args[3])))
-        local k=message.content:find(args[3])
-        message:reply(decrypt(message.content:sub(k),args[2]))
-    else
-        message:reply("wrong arguments! try "..data.prefix.."`encrypt {key} {value}` instead ")
-    end
-end,
-{
-    description = "decrypts a word",
-    example = "{prefix}decrypt amogus 767 769 767 767 773 684 750 749 759 749",
-    slowdown = 7
-}}
+return {
+    function(...)
+        local message, client, data = ...
+        local args = message.content:split(" ")
+        if args[3] then
+            print(message.content:sub(message.content:find(args[3])))
+            local k = message.content:find(args[3])
+            message:reply(decrypt(message.content:sub(k), args[2]))
+        else
+            message:reply("wrong arguments! try " .. data.prefix .. "`encrypt {key} {value}` instead ")
+        end
+    end,
+    {
+        description = "decrypts a word",
+        example = "{prefix}decrypt amogus 767 769 767 767 773 684 750 749 759 749",
+        slowdown = 7
+    }
+}
